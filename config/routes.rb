7ff22fb286856064d8102fcd89feb278c505d6d9
Dave Taylor <ddataylor@gmail.com>
2023-08-23 17:16:58 +0100

    Incorporate destroy and add links on the index page for show, edit and delete actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  # resources :articles, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
