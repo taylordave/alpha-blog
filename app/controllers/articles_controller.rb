e5afe2d208419be872adaf6ef819589868306502
Dave Taylor <ddataylor@gmail.com>
2023-08-23 16:45:42 +0100

    Build feature to allow for editing of existing articles and incorporate necessary error handling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -20,6 +24,17 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params.require(:article).permit(:title, :description))
+    if @article.save
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
