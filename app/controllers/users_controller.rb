9480c0719a6b515bc10fd75ba1ffb0300fd3ac5a
Dave Taylor <ddataylor@gmail.com>
2023-08-27 15:57:18 +0100

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
 
   def new
     @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    if @user.save
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -15,6 +29,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
